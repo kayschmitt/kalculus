867304f6c1f54b4203f7b1c66995f1508978b63c
kayschmitt <Home@News-MacBook-Pro.local>
2015-11-20 14:39:24 -0800

    submit

---
+++ js/Kalculus_files/creative.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<h1>ERROR 404</h1><br>
+Page not found.....
+</center>
+</body>
+</html>
---
+++ js/Kalculus_files/jquery-1.10.2.js
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>403 Forbidden</title>
+</head><body>
+<h1>Forbidden</h1>
+<p>You don't have permission to access /Kalculus/js/jquery-1.10.2.js
+on this server.</p>
+<hr>
+<address>Apache/2.4.7 (Ubuntu) Server at orca-48m03k23.cloudapp.net Port 80</address>
+</body></html>
